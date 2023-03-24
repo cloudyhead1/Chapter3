eece2d0a0616f5ddc0c69bfe42e9d2f1809a2921
ercx <ercx@fresnou.org>
2023-03-24 10:29:13 -0700

    array 1

---
+++ array_1.js
@@ -0,0 +1,18 @@
+let value = [3 ,4, 5, 6,];
+
+function printSecond(arr){
+    console.log( arr[3, 4, 5, 6] )
+
+}
+
+
+function printfirst(arr){
+
+    console.log( arr[0] )
+
+}
+
+
+printfirst( ['a', 'b', 'c', 'd'] )
+
+printfirst( [7, 5, 3, 1] )
