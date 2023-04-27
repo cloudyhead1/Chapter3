5ad9d01010ed1d2460024b054a80b6d4d8a1fdee
ercx <ercx@fresnou.org>
2023-04-27 10:19:00 -0700

    combo_menu

--- combo_menu.js
+++ combo_menu.js
@@ -2,17 +2,37 @@ const readLine = require('readline-sync');
 
 
 let myMenu = [];
+let myCost = [];
 console.log("Kinds of sandwich:  Chicken, Beef, Tofu");
+
 myMenu.push(readLine.question('Pick a sandwich: '));
+if (myMenu[0]=="Chicken" ){
+    myCost.push(5.25)
+} if (myMenu[0]=="Beef" ){
+    myCost.push(6.25)
+} if (myMenu[0]=="Tofu" ){
+    myCost.push(5.75);
 
 let drink_choice = (readLine.question('Want a Drink?: ')) ;
-if (drink_choice == "Yes"){ console.log(`"Drink size:  Small, Medium, Large"`);
-myMenu.push(readLine.question('Pick a size: '));}
 if (drink_choice == "yes"){ console.log(`"Drink size:  Small, Medium, Large"`);
     myMenu.push(readLine.question('Pick a size: '));}
-else {
-    console.log("k");
-}
+    if (myMenu[0]=="Small" ){
+        myCost.push(1.00)
+    } if (myMenu[0]=="Medium" ){
+        myCost.push(1.75)
+    } if (myMenu[0]=="Large" ){
+        myCost.push(2.25);
+if (drink_choice == "no") { console.log("k");}
 
+let fry_choice = (readLine.question('Want fries?: ')) ;
+if (fry_choice == "yes"){ console.log(`"fries size:  Small, Medium, Large"`);
+    myMenu.push(readLine.question('Pick a size: '));
+    if (myMenu[0]=="Small" ){
+        myCost.push(1.00)
+    } if (myMenu[0]=="Medium" ){
+        myCost.push(1.50)
+    } if (myMenu[0]=="Large" ){
+        myCost.push(2.00);
+if (fry_choice == "no") { console.log("k");}
 console.log(myMenu);
-
+console.log(myCost);
